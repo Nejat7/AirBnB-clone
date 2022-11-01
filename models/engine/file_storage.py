4204238a25b5c8f448b99eb3869f2ed0705de571
tok2fatsu <mailit2moi@gmail.com>
2022-11-01 12:19:35 -0700

    Fixing linking bug

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-<<<<<<< HEAD
 """Module file_storage
 This Module contains a definition for FileStorage Class
 """
@@ -16,36 +14,12 @@ class FileStorage:
     """FileStorage Class
     Attributes:
         __file_path (str): string - path to the JSON file
-=======
-=======
->>>>>>> 6e82d82d7a43d2fd7dc8a7171301ff513ec8f578
-"""Defines the FileStorage class."""
-import json
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.place import Place
-from models.amenity import Amenity
-from models.review import Review
-
-
-class FileStorage:
-    """Represent an abstracted storage engine.
-    Attributes:
-        __file_path (str): The name of the file to save objects to.
-<<<<<<< HEAD
->>>>>>> 6e82d82d7a43d2fd7dc8a7171301ff513ec8f578
-=======
->>>>>>> 6e82d82d7a43d2fd7dc8a7171301ff513ec8f578
         __objects (dict): A dictionary of instantiated objects.
     """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-<<<<<<< HEAD
-<<<<<<< HEAD
         """returns the dictionary __objects"""
         return self.__objects
 
@@ -71,36 +45,3 @@ class FileStorage:
         sub_module = re.sub('(?!^)([A-Z]+)', r'_\1', name).lower()
         module = importlib.import_module(f"models.{sub_module}")
         return getattr(module, name)
-=======
-=======
->>>>>>> 6e82d82d7a43d2fd7dc8a7171301ff513ec8f578
-        """Return the dictionary __objects."""
-        return FileStorage.__objects
-
-    def new(self, obj):
-        """Set in __objects obj with key <obj_class_name>.id"""
-        ocname = obj.__class__.__name__
-        FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
-
-    def save(self):
-        """Serialize __objects to the JSON file __file_path."""
-        odict = FileStorage.__objects
-        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
-        with open(FileStorage.__file_path, "w") as f:
-            json.dump(objdict, f)
-
-    def reload(self):
-        """Deserialize the JSON file __file_path to __objects, if it exists."""
-        try:
-            with open(FileStorage.__file_path) as f:
-                objdict = json.load(f)
-                for o in objdict.values():
-                    cls_name = o["__class__"]
-                    del o["__class__"]
-                    self.new(eval(cls_name)(**o))
-        except FileNotFoundError:
-            return
-<<<<<<< HEAD
->>>>>>> 6e82d82d7a43d2fd7dc8a7171301ff513ec8f578
-=======
->>>>>>> 6e82d82d7a43d2fd7dc8a7171301ff513ec8f578
